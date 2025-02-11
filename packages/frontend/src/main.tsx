d5e20d1c7c6ab9e99ef72629d0dd1f59877f26c6
Vipul Patel <vipul.patel@banastech.com>
2025-02-11 17:16:20 +0530

    Package geneated

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import App from './app/app';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
