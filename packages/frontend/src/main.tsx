eaaa50a600511bb5e27e290fdcf62c1bed088e33
chintan <chintanpatel1081@gmail.com>
2025-05-13 18:42:49 +0530

    paths working

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../src/App'
+import App from '@src/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
