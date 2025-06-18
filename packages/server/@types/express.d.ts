585d8894671b9a737dbdb31c02c103ca3351b54d
Vipul Patel <vipul.patel@banastech.com>
2025-06-18 18:11:13 +0530

    Workspace Schema

--- packages/server/@types/express.d.ts
+++ packages/server/@types/express.d.ts
@@ -1,21 +1,5 @@
-// declare module 'express-serve-static-core' {
-//   interface Request {
-//     workspaceId?: string;
-//     tenantId?: string;
-//   }
-// }
-
-
-// declare module "express-serve-static-core" {
-//   interface Request {
-//     workspaceId?: string;
-//     tenantId?: string;
-//   }
-// }
-
 declare namespace Express {
    export interface Request {
       workspaceId?: string;
-      tenantId?: string;
    }
 }
