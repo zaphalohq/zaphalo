4e43c0763ed6af946390c0a26391303010295658
Vipul Patel <patelvipul9979@gmail.com>
2025-09-23 16:53:33 +0530

    Broadcast preview

--- packages/frontend/src/pages/Broadcast.tsx
+++ packages/frontend/src/pages/Broadcast.tsx
@@ -4,14 +4,15 @@ import SubmitButton from '@src/components/UI/SubmitButton'
 import { useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import { FiArrowLeft } from 'react-icons/fi'
-
 import BroadcastList from "@src/modules/broadcast/components/BroadcastList";
 import BroadcastForm from "@src/modules/broadcast/components/BroadcastForm";
+import TemplatePreviewDialog from '@src/modules/whatsapp/components/templates/TemplatePreview';
 
 const Broadcast = () => {
   const [showForm, setShowForm] = useState(false);
   const [broadcast, setBroadcast] = useState(false);
   const [readOnly, setReadOnly] = useState(false);
+  const [preview, setPreview] = useState(false);
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -22,12 +23,17 @@ const Broadcast = () => {
           showForm={setShowForm}
           setReadOnly={setReadOnly}
           setBroadcast={setBroadcast}
+          setPreview={setPreview}
           onCreate={() => {
             setBroadcast(false)
             setReadOnly(false)
             setShowForm(true)
           }} />
       )}
+      {preview ? (
+          <TemplatePreviewDialog open={preview} setOpen={setPreview} templateId={preview}/>
+          ) : (<></>)
+        }
     </div>
   );
 }
